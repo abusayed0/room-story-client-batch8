babbf5e96d1a5a57661320d6e928f7e8aebb21eb
abusayed0 <abusayed48876@gmail.com>
2023-11-30 03:14:04 +0600

    added project readme file

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# React + Vite
+# [Room Story](https://room-story.netlify.app/)
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Fetures and fuctionalities :
+* 3 different user role Employee, Hr and Admin.
+* Employee can submit his/her task, see his/her monthly payment history.
+* Hr can verify, pay employee.can see employee submitted tasks.
+* Admin can fire any employee including Hr.
+* Employee, Hr, Admin 3 different dashboard.
+* Email/password authentication system.
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,22 +1,39 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 const axiosSucure = axios.create({
-    baseURL: "http://localhost:5000",
+  baseURL: "http://localhost:5000",
 });
 const useAxiosSecure = () => {
+  const navigate = useNavigate();
+  const {logOut} = useAuth();
+  axiosSucure.interceptors.request.use((config) => {
+    // Do something before request is sent
+    const token = localStorage.getItem("access-token");
+    console.log("request stopped by interceptors token :", token);
+    config.headers.authorization = `Bearer ${token}`;
+    return config;
+  }, (error) => {
+    // Do something with request error
+    return Promise.reject(error);
+  });
 
-    axiosSucure.interceptors.request.use( (config) => {
-        // Do something before request is sent
-        const token = localStorage.getItem("access-token");
-        console.log("request stopped by interceptors token :", token);
-        config.headers.authorization = `Bearer ${token}`;
-        return config;
-      }, (error)  => {
-        // Do something with request error
-        return Promise.reject(error);
-      });
+  axiosSucure.interceptors.response.use(response => {
+    return response;
+  }, async (error) => {
+    const status = error.response.status;
+    console.log("response status erro", error.response.status);
+    if (status === 401 || status === 403) {
+      await logOut()
+      navigate("/log-in");
+    }
+    return Promise.reject();
+  });
 
-    return axiosSucure;
+
+
+  return axiosSucure;
 };
 
 export default useAxiosSecure;
