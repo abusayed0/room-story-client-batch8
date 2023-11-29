8bcb6cabaaadaa1942e0f94ba9ac810a8c41e8cd
abusayed0 <abusayed48876@gmail.com>
2023-11-30 00:24:10 +0600

    added make hr and fired functionality

--- src/pages/dashboard/all-employee-list/all-employee-list/AllEmployeeList.jsx
+++ src/pages/dashboard/all-employee-list/all-employee-list/AllEmployeeList.jsx
@@ -1,10 +1,150 @@
+import { Button, Card, Typography } from "@material-tailwind/react";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
+import { FaCheck } from "react-icons/fa6";
+import toast from "react-hot-toast";
+import Swal from "sweetalert2";
+
+
 
 const AllEmployeeList = () => {
+    const axiosSucure = useAxiosSecure();
+    const { data: allEmployee = [], refetch } = useQuery({
+        queryKey: ["allEmployee"],
+        queryFn: async () => {
+            const res = await axiosSucure.get("all-employee-list");
+            return res.data;
+        }
+    });
+
+    const handleFired = id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, do!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSucure.patch(`/users/fired/${id}`)
+                    .then(res => {
+                        console.log(res.data);
+                        if (res.data.modifiedCount) {
+                            refetch();
+                            toast.success("Fired sucessfull !");
+                        }
+                    })
+            }
+        });
+
+
+    };
+
+    const handleMakeHr = id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, do!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                axiosSucure.patch(`/users/make-hr/${id}`)
+                    .then(res => {
+                        console.log(res.data);
+                        if (res.data.modifiedCount) {
+                            refetch();
+                            toast.success("Make Hr successfully!")
+                        }
+                    })
+            }
+        });
+    };
+
+
+    const TABLE_HEAD = ["Name", "Designation", "Make Hr", "Fire"];
+    const TABLE_ROWS = allEmployee;
     return (
-        <div>
-            <h2>This is  all employee list page !</h2>
+        <div className="mt-28">
+            <Card className="h-full w-full overflow-scroll">
+                <table className="w-full min-w-max table-auto text-left">
+                    <thead>
+                        <tr>
+                            {TABLE_HEAD.map((head) => (
+                                <th
+                                    key={head}
+                                    className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
+                                >
+                                    <Typography
+                                        variant="small"
+                                        color="blue-gray"
+                                        className="font-normal leading-none opacity-70"
+                                    >
+                                        {head}
+                                    </Typography>
+                                </th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {TABLE_ROWS.map(({ _id, name, designation, role, isFired }, index) => {
+                            const isLast = index === TABLE_ROWS.length - 1;
+                            const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
+
+                            return (
+                                <tr key={_id}>
+                                    <td className={classes}>
+                                        <Typography
+                                            variant="small"
+                                            color="blue-gray"
+                                            className="font-normal"
+                                        >
+                                            {name}
+                                        </Typography>
+                                    </td>
+                                    <td className={classes}>
+                                        <Typography
+                                            variant="small"
+                                            color="blue-gray"
+                                            className="font-normal"
+                                        >
+                                            {designation}
+                                        </Typography>
+                                    </td>
+                                    <td className={classes}>
+                                        <Typography
+                                            variant="small"
+                                            color="blue-gray"
+                                            className="font-normal"
+                                        >
+                                            {role === "hr" ? "Alread hr" : <Button onClick={() => handleMakeHr(_id)} size="sm" color="green"><FaCheck /></Button>
+                                            }
+                                        </Typography>
+                                    </td>
+                                    <td className={classes}>
+
+                                        {
+                                            isFired
+                                                ?
+                                                "Fired"
+                                                :
+                                                < Button onClick={() => handleFired(_id)} size="sm" color="red"><FaCheck /></Button >
+                                        }
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </Card>
         </div>
     );
 };
 
 export default AllEmployeeList;
+
--- src/pages/dashboard/payment-history/payment-history/PaymentHistory.jsx
+++ src/pages/dashboard/payment-history/payment-history/PaymentHistory.jsx
@@ -1,9 +1,10 @@
-import { Card, Typography } from "@material-tailwind/react";
+import { Button, Card, CardFooter, Typography } from "@material-tailwind/react";
 
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../../hooks/useAuth";
 import useAxiosSecure from "../../../../hooks/useAxiosSecure";
 import moment from "moment";
+import { useState } from "react";
 
 const PaymentHistory = () => {
     const { user } = useAuth();
@@ -17,22 +18,32 @@ const PaymentHistory = () => {
     })
     // console.log({paymentHistory});
     const TABLE_HEAD = ["Month", "Amount", "Transaction Id", "Paid By"];
-    const sortByRecent = paymentHistory.sort((a, b) =>{
+    const sortByRecent = paymentHistory.sort((a, b) => {
         const dateString1 = a.paymentFor;
         const dateString2 = b.paymentFor;
-        const dateMoment1 = moment(dateString1,"MMM, yyy");
-        const dateMoment2 = moment(dateString2,"MMM, yyy");
+        const dateMoment1 = moment(dateString1, "MMM, yyy");
+        const dateMoment2 = moment(dateString2, "MMM, yyy");
         // console.log("a", dateMoment1);
         // console.log("b", dateMoment2);
         // console.log(dateMoment2.isBefore(dateMoment1));
-        if(dateMoment2.isBefore(dateMoment1)){
+        if (dateMoment2.isBefore(dateMoment1)) {
             return -1;
         }
         return 1;
     });
-
+    
     // console.log(sortByRecent);
     const TABLE_ROWS = sortByRecent;
+    const totalRows = TABLE_ROWS.length;
+    const rowPerPage = 5;
+    const totalPage = Math.ceil(totalRows/ rowPerPage);
+    const [currentPage, setCurrentPage] = useState(0);
+    const handlePrev = () => {
+        setCurrentPage(currentPage - 1)
+    };
+    const handleNext = () => {
+        setCurrentPage(currentPage + 1);
+    };
     return (
         <div className="mt-28">
             <Card className="h-full w-full overflow-scroll">
@@ -56,7 +67,7 @@ const PaymentHistory = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {TABLE_ROWS.map(({ paymentFor, paidAmount, transactionId, paidBy}, index) => {
+                        {TABLE_ROWS.slice(rowPerPage * currentPage, (rowPerPage * currentPage) + rowPerPage).map(({ paymentFor, paidAmount, transactionId, paidBy }, index) => {
                             const isLast = index === TABLE_ROWS.length - 1;
                             const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
 
@@ -98,12 +109,25 @@ const PaymentHistory = () => {
                                             {paidBy}
                                         </Typography>
                                     </td>
-                                    
+
                                 </tr>
                             );
                         })}
                     </tbody>
                 </table>
+                <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4">
+                    <Typography variant="small" color="blue-gray" className="font-normal">
+                        Page {currentPage + 1} of {totalPage}
+                    </Typography>
+                    <div className="flex gap-2">
+                        <Button disabled={currentPage === 0} onClick={handlePrev} variant="outlined" size="sm">
+                            Previous
+                        </Button>
+                        <Button disabled={currentPage === totalPage -1} onClick={handleNext} variant="outlined" size="sm">
+                            Next
+                        </Button>
+                    </div>
+                </CardFooter>
             </Card>
         </div>
     );
